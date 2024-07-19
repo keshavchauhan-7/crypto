ea1fa41899d0d595a8f7f9bdc8ac2922ac1f85b0
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-07-19 10:12:26 +0530

    create new pages

--- Cryptotracking/src/App.jsx
+++ Cryptotracking/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Coin from './pages/Coin/Coin'
+
+
 
 const App = () => {
   return (
     <div className='app'>
-      <Navbar/>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Coin />} />
+      </Routes>
     </div>
   )
 }
