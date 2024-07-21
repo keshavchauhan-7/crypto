1149f49d3f2b8f46ba28e4710644f36edd9dac1f
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-07-21 11:47:36 +0530

    styling coin

--- Cryptotracking/src/pages/Coin/Coin.jsx
+++ Cryptotracking/src/pages/Coin/Coin.jsx
@@ -2,12 +2,13 @@ import React, { useContext, useEffect, useState } from 'react'
 import './Coin.css'
 import { useParams } from 'react-router-dom'
 import { CoinContext } from '../../context/CoinContext';
+import LineChart from '../../components/LineChart/LineChart';
 
 const Coin = () => {
   const { coinId } = useParams();
   const [coinData, setCoinData] = useState();
-  const { currency } = useContext(CoinContext);
   const [historicalData, setHistoricalData] = useState();
+  const { currency } = useContext(CoinContext);
 
 
   const fetchCoinData = async () => {
@@ -22,7 +23,7 @@ const Coin = () => {
   const fetchHistoricalData = async () => {
     const options = { method: 'GET', headers: { accept: 'application/json' } };
 
-    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10`, options)
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`, options)
       .then(response => response.json())
       .then(response => setHistoricalData(response))
       .catch(err => console.error(err));
@@ -30,15 +31,42 @@ const Coin = () => {
 
   useEffect(() => {
     fetchCoinData();
+    fetchHistoricalData();
   }, [currency])
 
 
-  if (coinData, historicalData) {
+  if (coinData&&historicalData) {
     return (
       <div className='coin'>
         <div className="coin-name">
           <img src={coinData.image.large} alt="" />
           <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+        </div>
+        <div className='coin-chart'>
+          <LineChart historicalData={historicalData} />
+        </div>
+
+        <div className="coin-info">
+          <ul>
+            <li>Crypto Market Rank</li>
+            <li>{coinData.market_cap_rank}</li>
+          </ul>
+          <ul>
+            <li>Current Price</li>
+            <li> {currency.symbol} {coinData.market_data.current_price[currency.name].toLocaleString()}</li>
+          </ul>
+          <ul>
+            <li>Market cap</li>
+            <li> {currency.symbol} {coinData.market_data.market_cap[currency.name].toLocaleString()}</li>
+          </ul>
+          <ul>
+            <li>24 Hour high</li>
+            <li> {currency.symbol} {coinData.market_data.high_24h[currency.name].toLocaleString()}</li>
+          </ul>
+          <ul>
+            <li>24 Hour low</li>
+            <li> {currency.symbol} {coinData.market_data.low_24h[currency.name].toLocaleString()}</li>
+          </ul>
         </div>
       </div>
     )
